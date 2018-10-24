3a726e958358536dc67605322077da2b1f8dcbc0
flpegomes <felipegomesfurtado@gmail.com>
2018-10-24 01:23:39 -0300

    começo do post edit

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -20,6 +20,10 @@ import { Link } from 'react-router-dom'
 import CardActionArea from '@material-ui/core/CardActionArea';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+
 
 const styles = {
     paper: {
@@ -83,15 +87,52 @@ class Post extends Component {
         this.setState({ anchorEl: event.currentTarget });
     };
 
-    handleClose = () => {
-        this.setState({ anchorEl: null });
+    handleChange = name => event => {
+        this.setState({
+            editPost: { ...this.state, [name]: event.target.value }
+        });
+    };
+
+    handleEdit = (post) => {
+        this.setState({ 
+            isEditing: true,
+            anchorEl: null,
+            editPost: {
+                title: post.title,
+                body: post.body
+            }
+        });
     };
     
+    state = {
+        isEditing: false,
+        anchorEl: null,
+        editPost : {
+            title: '',
+            body: ''
+        }
+    }
+
+    _verifyFields = (countTitle, countBody) => {
+        if(countTitle < 50 && countBody < 300 ){
+            if(countTitle >= 0 && countBody >= 0) {
+                return false  
+            } else {
+                return true
+            }
+        } else {
+            return true
+        }
+    }
 
     render() {
         
         const { classes, post } = this.props   
-        const { anchorEl } = this.state;
+        const { anchorEl, isEditing } = this.state;
+        const { title, body } = this.state.editPost
+        const countBody = 300 - body.length;
+        const countTitle = 50 - title.length;
+
         return (                
                 <Card raised={false} className={classes.card}>
                     <CardHeader
@@ -120,7 +161,7 @@ class Post extends Component {
                                     open={Boolean(anchorEl)}
                                     onClose={this.handleClose}
                                 >
-                                    <MenuItem onClick={this.handleClose}>Edit</MenuItem>
+                                    <MenuItem onClick={() => this.handleEdit(post)}>Edit</MenuItem>
                                     <MenuItem onClick={this.handleClose}>Delete</MenuItem>
                                 </Menu>
                             </div>
@@ -136,19 +177,108 @@ class Post extends Component {
                                 </Typography>
                             }
                     />
-                    <CardActionArea
-                        component={Link}
-                        to={`/${post.category}/${post.id}`}
-                    >
+                    {isEditing ? (
                         <CardContent>
-                            <Typography variant='h6' component="p">
-                                {post.title}
-                            </Typography>
-                            <Typography variant='subtitle1' component="p">
-                                {post.body}
-                            </Typography>
+                            <div style={{margin: 16}}>
+                                <div style={{flexWrap: 'wrap', display: 'flex'}}>
+                                
+                                    <TextField
+                                        id="standard-name"
+                                        label="Enter a title for your post"
+                                        className={classes.textField}
+                                        value={title}
+                                        onChange={this.handleChange('title')}
+                                        margin="normal"
+                                        variant="filled"
+                                        helperText={(
+                                            <span className={classes.root}>
+                                                <span className={classes.grow}>
+                                                </span>
+                                                <span>
+                                                    {countTitle < 20 ? (
+                                                        <span style={{color:'#ff0000'}}>
+                                                            {countTitle}
+                                                        </span>
+                                                    ) 
+                                                    :(
+                                                        <span>
+                                                            {countTitle}
+                                                        </span>
+                                                    )}
+                                                
+                                                </span>
+                                            </span>
+                                            
+                                        )}
+                                        error={countTitle < 0 ? true : false}
+                                        
+                                    />
+
+                                    <TextField
+                                        id="standard-body"
+                                        label="type your post"
+                                        placeholder="write anything here..."
+                                        onChange={this.handleChange('body')}
+                                        value={body}
+                                        margin="normal"
+                                        variant="filled"
+                                        fullWidth
+                                        multiline
+                                        rowsMax="4"
+                                        rows="2"
+                                        helperText={(
+                                            <span className={classes.root}>
+                                                <span className={classes.grow}>
+                                                </span>
+                                                <span>
+                                                    {countBody < 20 ? (
+                                                        <span style={{color:'#ff0000'}}>
+                                                            {countBody}
+                                                        </span>
+                                                    ) 
+                                                    :(
+                                                        <span>
+                                                            {countBody}
+                                                        </span>
+                                                    )}
+                                                
+                                                </span>
+                                            </span>
+                                            
+                                        )}
+                                        
+                                        error={countBody < 0 ? true : false}
+                                    />
+
+                                    <Button 
+                                        variant="contained" 
+                                        color="primary" 
+                                        className={classes.button} 
+                                        disabled={this._verifyFields(countTitle, countBody)}
+                                        onClick={() => {}}
+                                    >
+                                        SEND
+                                    </Button>
+                                </div>
+                            </div>
+                            
+                            
                         </CardContent>
-                    </CardActionArea>
+                    ) : ( 
+                        <CardActionArea
+                            component={Link}
+                            to={`/${post.category}/${post.id}`}
+                        >
+                            <CardContent>
+                                <Typography variant='h6' component="p">
+                                    {post.title}
+                                </Typography>
+                                <Typography variant='subtitle1' component="p">
+                                    {post.body}
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    )}
                         
                     
                     <CardActions className={classes.actions}>
